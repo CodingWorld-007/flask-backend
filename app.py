dd65bda96757e706a3601c93e71e76bcbe75438e
CodingWorld-007 <codingworld.official.007@gmail.com>
2025-03-24 22:44:00 +0530

    Updated app.py, requirements.txt & removed unused files

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def submit_attendance():
     else:
         return jsonify({"status": "error", "message": "Duplicate entry detected"}), 409
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
